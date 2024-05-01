88bf2770f7d96f786c4534a96e2b605623625044
Adi-070 <mishraaditya760@gmail.com>
2024-05-01 19:58:19 +0530

    one warning sorted

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -34,7 +34,7 @@ const Layout = ({ children }) => {
       </nav>
   
       {/* Get started button */}
-      <button type="button" class="text-white bg-black focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium text-sm px-5 py-2.5 mr-2 mb-2">
+      <button type="button" className="text-white bg-black focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium text-sm px-5 py-2.5 mr-2 mb-2">
         Get started
       </button>
     </header>
